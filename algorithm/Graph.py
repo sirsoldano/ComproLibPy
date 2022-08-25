944ba3d472a7ced72d440f3e8df8d4693b17e49c
Daiki Soma <fg.strato@gmail.com>
2022-08-25 12:55:42 +0900

    Update Graph.py

--- algorithm/Graph.py
+++ algorithm/Graph.py
@@ -13,9 +13,9 @@ def dfs(pos):
 	node[pos] = 1
 	for p in edge[pos]:
 		dfs(p) if node[p]==0 else None
-def dfs(pos):
-	stack=[pos]
-	node[pos]=1
+def dfs(s):
+	stack=[s]
+	node[s]=1
 	while len(stack)>0:
 		pos = stack.pop()
 		for p in edge[pos]:
@@ -24,17 +24,28 @@ def dfs(pos):
 				node[p]=1
 # tc : N+M
 import queue
-def bfs(pos):
+def bfs(s):
 	q = queue.Queue()
-	q.put(pos)
-	node[pos]=0
+	q.put(s)
+	node[s]=0
 	while not q.empty():
 		pos = q.get()
 		for p in edge[pos]:
 			if node[p]==-1:
 				q.put(p)
 				node[p]=node[pos]+1
-
+# Dijkstra法
+import heapq
+edge = [{nn:dist} for n in range(N)]
+def dijkstra(s):
+	q = []
+	heapq.heappush(q,(0,s))
+	while len(q)>0 :
+		pos = heapq.heappop(q)
+		for p,dist in edge[pos[1]].items():
+			if node[p]==-1 or node[p]>pos[0]+dist:
+				node[p] = pos[0]+dist
+				heapq.heappush(q,(node[p],p))
 # DSmaze
 H,W = map(int,input().split())
 edge = [list() for n in range(H*W)]
