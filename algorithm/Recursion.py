233f49e73dc9225a5a6625a5df5fdb59248857a7
Daiki Soma <fg.strato@gmail.com>
2022-08-25 16:21:35 +0900

    Update Recursion.py

--- algorithm/Recursion.py
+++ algorithm/Recursion.py
@@ -1,7 +1,6 @@
 # 分割統治法
 def DivCon(l,r) :
-	if r-l==1 :
-		return A[l]
+	if r-l==1 : return A[l]
 	m = (l+r)//2
 	return divcon(l,m)+divcon(m,r)
 
@@ -9,8 +8,7 @@ def DivCon(l,r) :
 # tc : NlogN
 # ul : 10^6
 def MergeSort(l,r) :
-	if r-l==1 :
-		return
+	if r-l==1 : return
 	m = (l+r)//2
 	MergeSort(l,m); MergeSort(m,r)
 	ordered,c1,c2,cnt = [0]*(r-l),l,m,0
@@ -22,5 +20,4 @@ def MergeSort(l,r) :
 			ordered[cnt] = A[c1]
 			c1+=1
 		cnt+=1
-	for c in range(cnt):
-		A[l+c]=ordered[c]
+	for c in range(cnt) : A[l+c]=ordered[c]
