07449d29f5550f72263e2956293af3b08467c89c
Daiki Soma <fg.strato@gmail.com>
2022-08-25 16:17:50 +0900

    Create Mod.py

---
+++ algorithm/Mod.py
@@ -0,0 +1,30 @@
+# b^(M-1) ≡ 1 (mod M) M:prime, 1 <= b < M
+# b*modular ≡ 1 (mod M) ≡ b^(M-1)
+# modular = b^(M-2) % M
+
+
+class MOD:
+    def __init__(self,mod):
+        self.mod = mod
+        self.F = []
+    def modpow(self,a,b):
+        ans = 1
+        while b>0:
+            if b&1 : ans=ans*a%self.mod
+            b>>=1
+            a=a*a%self.mod
+        return ans
+    def modC(self,x,y):
+        if self.F:
+            fx,fy,fxy = self.F[x],self.F[y],self.F[x+y]
+        else:
+            fx,fy,fxy = 1,1,1
+            for x in range(1,x+1) : fx = fx*x%self.mod
+            for y in range(1,y+1) : fy = fy*y%self.mod
+            for xy in range(1,x+y+1) : fxy = fxy*xy%self.mod
+        fx = self.modpow(fx,self.mod-2)
+        fy = self.modpow(fy,self.mod-2)
+        return fxy*fx%self.mod*fy%self.mod
+    def modF(self,N):
+        self.F = [1]*(N+1)
+        for n in range(1,N+1) : self.F[n] = self.F[n-1]*n
