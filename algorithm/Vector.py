2cf48be17b2712e596abf6bfb13f6e19ce8220d2
Daiki Soma <fg.strato@gmail.com>
2022-08-27 11:38:46 +0900

    Update Vector.py

--- algorithm/Vector.py
+++ algorithm/Vector.py
@@ -1,20 +1,20 @@
 # distance**2
 def distpow(axy,bxy=(0,0)) :
-	return (axy[0]-bxy[0])**2 + (axy[1]-bxy[1])**2
+    return (axy[0]-bxy[0])**2 + (axy[1]-bxy[1])**2
 # inner product ( <90:innnerP>0, >90:innerP<0, =90:innerP=0 )
 def innerP(axy,bxy,cxy=(0,0),dxy=(0,0)) :
-	return (axy[0]-cxy[0])*(bxy[0]-dxy[0]) + (axy[1]-cxy[1])*(bxy[1]-dxy[1])
+    return (axy[0]-cxy[0])*(bxy[0]-dxy[0]) + (axy[1]-cxy[1])*(bxy[1]-dxy[1])
 # outer product ( outerP(a,b)>0:ω(a)<ω(b) outerP(a,b)<0:ω(a)>ω(b) outerP(a,b)=0: inline )
 # abs(outerP) = area of parallelogram
 def outerP(axy,bxy,cxy=(0,0),dxy=(0,0)) :
-	return (axy[0]-cxy[0])*(bxy[1]-dxy[1]) - (axy[1]-cxy[1])*(bxy[0]-dxy[0])
+    return (axy[0]-cxy[0])*(bxy[1]-dxy[1]) - (axy[1]-cxy[1])*(bxy[0]-dxy[0])
 # 線分abと線分cdの交差判定
 if outerP(axy,dxy,cxy,cxy)==0 and outerP(bxy,dxy,cxy,cxy)==0:
     if max(min(axy[0],bxy[0]),min(cxy[0],dxy[0]))<=min(max(axy[0],bxy[0]),max(cxy[0],dxy[0])) and \
-	max(min(axy[1],bxy[1]),min(cxy[1],dxy[1]))<=min(max(axy[1],bxy[1]),max(cxy[1],dxy[1])):
-		True
+    max(min(axy[1],bxy[1]),min(cxy[1],dxy[1]))<=min(max(axy[1],bxy[1]),max(cxy[1],dxy[1])):
+        True
 elif outerP(axy,dxy,cxy,cxy)*outerP(bxy,dxy,cxy,cxy)<=0 and outerP(cxy,bxy,axy,axy)*outerP(dxy,bxy,axy,axy)<=0:
-	True
+    True
 
 # 最近点対問題(分割統治法)
 # 凸包の構築(Andrewのアルゴリズム)
