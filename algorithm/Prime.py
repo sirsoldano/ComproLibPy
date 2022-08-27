6820ab44441d1777b048a496aee8cd58d5494550
Daiki Soma <fg.strato@gmail.com>
2022-08-27 11:35:17 +0900

    Update Prime.py

--- algorithm/Prime.py
+++ algorithm/Prime.py
@@ -1,51 +1,46 @@
 # tc : NloglogN
 # ul : 10^7
 def erast(N):
-	prime,is_prime,p = [],[0,0]+[1]*(N-1),2
-	while p<=N:
-		if is_prime[p]:
-			prime.append(p)
-			for i in range(2,N//p+1) : is_prime[i*p]=0
-		p+=1
-	return prime
+    prime,is_prime,p = [],[0,0]+[1]*(N-1),2
+    while p<=N:
+        if is_prime[p]:
+            prime.append(p)
+            for i in range(2,N//p+1) : is_prime[i*p]=0
+        p+=1
+    return prime
 # 約数の個数を事前列挙
 # tc : NlogN
 # ul : 10^7
 def divisors(N):
-	div = [0,1]+[2]*(N-1)
-	for n in range(2,N//2+1):
-	    i=2
-	    while n*i<=N:
-			div[n*i]+=1
-			i+=1
-	return div
+    div = [0,1]+[2]*(N-1)
+    for n in range(2,N//2+1):
+        i=2
+        while n*i<=N:
+            div[n*i]+=1
+            i+=1
+    return div
 # tc : √N
 # ul : 10^12
 def tridiv(n):
-	pf = []
-	f = 2
-	while f*f<=n:
-		if n%f == 0 :
-			pf.append(f)
-			n//=f
-		else:
-			f = f+2 if f>2 else 3
-	if n>1 : pf.append(n)
-	return pf
+    pf, f = [], 2
+    while f*f<=n:
+        if n%f == 0 :
+            pf.append(f)
+            n//=f
+        else : f = f+2 if f>2 else 3
+    if n>1 : pf.append(n)
+    return pf
 # 複数回実行する場合は事前に素数列挙
 # PL=erast(int(n**0.5))
 def tridivPL(n,PL):
-	pf = []
-	ind=0
-	while ind<len(PL) and PL[ind]*PL[ind]<=n:
-		if n%PL[ind] == 0 :
-			pf.append(PL[ind])
-			n//=PL[ind]
-		else:
-			ind+=1
-	if n>1 : pf.append(n)
-	return pf
-
+    pf, ind = [], 0
+    while ind<len(PL) and PL[ind]*PL[ind]<=n:
+        if n%PL[ind] == 0 :
+            pf.append(PL[ind])
+            n//=PL[ind]
+        else : ind+=1
+    if n>1 : pf.append(n)
+    return pf
 
 import collections
 collections.Counter(tridiv(int(input())))
@@ -54,13 +49,13 @@ collections.Counter(tridiv(int(input())))
 # tc : log(A+B)
 # ul : over 10^18
 def gcd(A,B):
-	while A>=1 and B>=1 :
-		if(A>B) : A = A%B
-		else : B = B%A
-	return A if A else B
+    while A>=1 and B>=1 :
+        if(A>B) : A = A%B
+        else : B = B%A
+    return A if A else B
 def gcd(A,B):
-	if B==0 : return A
-	else : return gcd(B,A%B)
+    if B==0 : return A
+    else : return gcd(B,A%B)
 # use library
 import math
 math.gcd(A,B)
@@ -69,8 +64,8 @@ lcm = A*B//math.gcd(A,B)
 # 倍数の集合 ( N//lcm を集合個数奇数個は加算、偶数個は減算してbit全探索)
 # https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_be
 for b in range(1,(1<<K)):
-	cnt, lcm = 0, 1
-	for k in range(K) : 
-		if b&(1<<k) : cnt, lcm = cnt+1, lcm*A[k]//math.gcd(lcm,A[k])
-	ans = ans + N//lcm if cnt%2 else ans - N//lcm
+    cnt, lcm = 0, 1
+    for k in range(K) : 
+        if b&(1<<k) : cnt, lcm = cnt+1, lcm*A[k]//math.gcd(lcm,A[k])
+    ans = ans + N//lcm if cnt%2 else ans - N//lcm
 print(ans)
