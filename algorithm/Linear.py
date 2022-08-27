845450645fa3cf372df197a53d663d13dfd65219
Daiki Soma <fg.strato@gmail.com>
2022-08-27 11:30:48 +0900

    Update Linear.py

--- algorithm/Linear.py
+++ algorithm/Linear.py
@@ -1,17 +1,17 @@
 class Linear:
-	def __init__(self,mod):
-		self.mod = mod
-	def modpowL(self,a,p):
-		ans = [[1 if m==n else 0 for m in range(len(a))] for n in range(len(a))]
-		while p>0:
-			if p&1 : ans = self.mulmodL(ans,a)
-			p>>=1
-			a = self.mulmodL(a,a)
-		return ans
-	def mulmodL(self,a,b):
-		c = [[0 for m in range(len(b))] for n in range(len(a))]
-		for i in range(len(a)):
-			for k in range(len(a)):
-				for j in range(len(a)):
-					c[i][j] = (c[i][j]+a[i][k]*b[k][j])%self.mod
-		return c
+    def __init__(self,mod):
+        self.mod = mod
+    def modpowL(self,a,p):
+        ans = [[1 if m==n else 0 for m in range(len(a))] for n in range(len(a))]
+        while p>0:
+            if p&1 : ans = self.mulmodL(ans,a)
+            p>>=1
+            a = self.mulmodL(a,a)
+        return ans
+    def mulmodL(self,a,b):
+        c = [[0 for m in range(len(b))] for n in range(len(a))]
+        for i in range(len(a)):
+            for k in range(len(a)):
+                for j in range(len(a)):
+                    c[i][j] = (c[i][j]+a[i][k]*b[k][j])%self.mod
+        return c
