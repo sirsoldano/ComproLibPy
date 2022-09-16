1cf31ff43ae1a99ebd7458e1277d2112fca7bf41
Daiki Soma <fg.strato@gmail.com>
2022-09-16 18:38:36 +0900

    Create UF.py

---
+++ struct/UF.py
@@ -0,0 +1,13 @@
+# 階層なしUF木
+class UnionFind:
+    def __init__(self,n):
+        self.uft = [-1]*n
+    def root(self,pos):
+        if self.uft[pos] == -1 : return pos
+        self.uft[pos]=self.root(self.uft[pos])
+        return self.uft[pos]
+    def union(self,a,b):
+        ra,rb = self.root(a),self.root(b)
+        if ra!=rb : self.uft[ra]=rb
+    def same(self,a,b):
+        return self.root(a)==self.root(b)
