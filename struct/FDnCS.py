b3680b5de3af765bebfc981033afe65e2f19eceb
Daiki Soma <fg.strato@gmail.com>
2022-09-30 07:30:33 +0900

    Update FDnCS.py

--- struct/FDnCS.py
+++ struct/FDnCS.py
@@ -8,3 +8,5 @@ def cs(A):
     CS = [A[0]]+[0]*(N-1)
     for n in range(1,N) : CS[n] = CS[n-1] + A[n]
     return CS
+
+# 領域追加は二次元いもす法 https://github.com/E869120/kyopro_educational_90/blob/main/editorial/028.jpg
