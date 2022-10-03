735be621f91193f121f4f1cfb657ad2622ca3134
Daiki Soma <fg.strato@gmail.com>
2022-10-03 18:16:26 +0900

    Update Segment.py

--- struct/Segment.py
+++ struct/Segment.py
@@ -14,40 +14,47 @@ class fenT:
             ans += self.fen[n]
             n -= n&-n
         return ans
-
 # セグメント木
+
+# 遅延セグメント木
 # 解説 https://algo-logic.info/segment-tree/
 # 競プロ典型 https://github.com/E869120/kyopro_educational_90/blob/main/editorial/029-02.jpg
-import sys
-sys.setrecursionlimit(100000)
 class segT:
     def __init__(self,N):
         self.slen = 1
         while(self.slen<N) : self.slen<<=1
-        self.st = [0] * (self.slen*2)
-        self.lz = [0] * (self.slen*2)
+        self.st = [-1] * (self.slen*2)
+        self.lz = [-1] * (self.slen*2)
     def deval(self,k):
         if k < self.slen:
             self.lz[k*2]=max(self.lz[k],self.lz[k*2])
             self.lz[k*2+1]=max(self.lz[k],self.lz[k*2+1])
         self.st[k]=max(self.st[k],self.lz[k])
-        self.lz[k]=0
-    def update(self,l,r,h,k,tl,tr):
+        self.lz[k]=-1
+    def updatePoint(self,i,x):
+        i += self.slen
+        self.st[i] = x
+        while i>=2 :
+            i>>=1
+            self.st[i] = max(self.st[i*2],self.st[i*2+1])
+    def updateSect(self,l,r,h) : return self._update(l,r,h,1,0,self.slen)
+    def _update(self,l,r,h,k,tl,tr):
         self.deval(k)
-        if tr<=l or r<=tl : return
         if l<=tl and tr<=r :
             self.lz[k]=h
             self.deval(k)
             return
-        self.update(l,r,h,k*2,tl,(tl+tr)//2)
-        self.update(l,r,h,k*2+1,(tl+tr)//2,tr)
-        self.st[k] = max(self.st[k*2],self.st[k*2+1])
-    def getmax(self,l,r,k,tl,tr):
+        elif tr<=l or r<=tl : return
+        else:
+            self._update(l,r,h,k*2,tl,(tl+tr)//2)
+            self._update(l,r,h,k*2+1,(tl+tr)//2,tr)
+            self.st[k] = max(self.st[k*2],self.st[k*2+1])
+    def getmax(self,l,r) : return self._getmax(l,r,1,0,self.slen)
+    def _getmax(self,l,r,k,tl,tr):
         self.deval(k)
-        if tr<=l or r<=tl : return 0
-        elif l<=tl and tr<=r : return self.st[k]
+        if l<=tl and tr<=r : return self.st[k]
+        elif tr<=l or r<=tl : return -1
         else :
-            lc = self.getmax(l,r,k*2,tl,(tl+tr)//2)
-            rc = self.getmax(l,r,k*2+1,(tl+tr)//2,tr)
+            lc = self._getmax(l,r,k*2,tl,(tl+tr)//2)
+            rc = self._getmax(l,r,k*2+1,(tl+tr)//2,tr)
             return max(lc,rc)
-
