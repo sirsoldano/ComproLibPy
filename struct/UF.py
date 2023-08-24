477f877074f34104287519daafd8a4a7a0347835
Daiki Soma <fg.strato@gmail.com>
2023-08-24 13:21:39 +0900

    Update UF.py

--- struct/UF.py
+++ struct/UF.py
@@ -2,15 +2,19 @@
 class UnionFind:
     def __init__(self,n):
         self.uft = [-1]*n
-    def root(self,pos):
+    def find(self,pos):
         if self.uft[pos] == -1 : return pos
-        self.uft[pos]=self.root(self.uft[pos])
+        self.uft[pos]=self.find(self.uft[pos])
         return self.uft[pos]
     def union(self,a,b):
-        ra,rb = self.root(a),self.root(b)
-        if ra!=rb : self.uft[ra]=rb
+        ra,rb = self.find(a),self.find(b)
+        if ra!=rb : 
+            if self.uft[ra]>self.uft[rb] : ra,rb = rb,ra
+            self.uft[ra] += self.uft[rb]
+            self.uft[rb] = ra
+        return 0
     def same(self,a,b):
-        return self.root(a)==self.root(b)
+        return self.find(a)==self.find(b)
 
 # 最少全域木
 items.sort()
