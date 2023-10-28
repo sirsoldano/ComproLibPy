ed9230847cb83b45b8ad2ec8a29c33ff0cea120a
Daiki Soma <fg.strato@gmail.com>
2023-10-28 12:55:39 +0900

    Update Mod.py

--- algorithm/Mod.py
+++ algorithm/Mod.py
@@ -8,13 +8,6 @@ class MOD:
         self.mod = mod
         self.F = []
         self.Finv = []
-    def modpow(self,a,p):
-        ans = 1
-        while p>0:
-            if p&1 : ans=ans*a%self.mod
-            p>>=1
-            a=a*a%self.mod
-        return ans
     def modC(self,X,Y):
         if self.F:
             return self.F[X+Y]*self.Finv[X]%self.mod*self.Finv[Y]%self.mod
@@ -23,13 +16,14 @@ class MOD:
             for x in range(1,X+1) : fx = fx*x%self.mod
             for y in range(1,Y+1) : fy = fy*y%self.mod
             for xy in range(1,X+Y+1) : fxy = fxy*xy%self.mod
-            deno = self.modpow(fx*fy%self.mod,self.mod-2)
+            deno = pow(fx*fy%self.mod,self.mod-2,self.mod)
             return fxy*deno%self.mod
     def modF(self,N):
         self.F = [1]*(N+1)
         for n in range(1,N+1) : self.F[n] = self.F[n-1] * n % self.mod
         self.Finv = [0]*(N+1)
-        for n in range(0,N+1) : self.Finv[n] = self.modpow(self.F[n], self.mod-2)
+        for n in range(0,N+1) : self.Finv[n] = pow(self.F[n], self.mod-2,self.mod)
+
 
 
 class MOD:
