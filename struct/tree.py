9f87f760971389feb034dccfa63b094d77044779
Daiki Soma <fg.strato@gmail.com>
2024-03-04 10:43:44 +0900

    Update tree.py

--- struct/tree.py
+++ struct/tree.py
@@ -13,32 +13,26 @@ def dfs(pos):
 # 根付き木へのダブリングで最近共通祖先
 class DubTree:
     def __init__(self,N):
-        self.N = N
-        self.K = 1
+        self.N, self.K = N, 1
         while (1<<self.K)<N : self.K += 1
+        self.node,self.wei = [None]*self.N, [None]*self.N
         self.edge = [[] for n in range(self.N)]
-        self.node = [-1]*self.N
-        self.id = []
-        self.parent = [[-1]*N for k in range(self.K)]
-    def append_edge(self,a,b):
-        self.edge[a].append(b)
-        self.edge[b].append(a)
+        self.parent = [[None]*N for k in range(self.K)]
+    def append_edge(self,a,b,w=1):
+        self.edge[a].append((b,w)); self.edge[b].append((a,w))
     def dfs(self,s):
-        stack=[s]
-        self.node[s] = 0
-        self.parent[0][s] = -1
+        stack, self.node[s],self.wei[s], self.parent[0][s] = [s], 0, 0, None
         while len(stack)>0:
             pos = stack.pop()
-            self.id.append(pos)
-            for p in self.edge[pos]:
-                if self.node[p]==-1:
+            for p,w in self.edge[pos]:
+                if self.node[p] is None:
                     stack.append(p)
-                    self.node[p] = self.node[pos]+1
+                    self.node[p], self.wei[p] = self.node[pos]+1, self.wei[pos]+w
                     self.parent[0][p] = pos
     def doubling(self):
         for k in range(self.K-1):
             for n in range(self.N):
-                if self.parent[k][n] == -1 : self.parent[k+1][n] = -1
+                if self.parent[k][n] is None : self.parent[k+1][n] = None
                 else : self.parent[k+1][n] = self.parent[k][self.parent[k][n]]
     def lca(self,a,b):
         if self.node[a]<self.node[b] : a,b=b,a
@@ -49,3 +43,6 @@ class DubTree:
             if self.parent[k][a]!=self.parent[k][b] : 
                 a,b = self.parent[k][a],self.parent[k][b]
         return self.parent[0][a]
+    def dist(self,a,b):
+        lca = self.lca(a,b)
+        return self.wei[a] + self.wei[b] - 2*self.wei[lca]
