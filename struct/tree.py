405d30abe4d0dab1ce80b0468ccb591a3fa69339
Daiki Soma <fg.strato@gmail.com>
2024-03-04 14:13:20 +0900

    Update tree.py

--- struct/tree.py
+++ struct/tree.py
@@ -46,3 +46,67 @@ class DubTree:
     def dist(self,a,b):
         lca = self.lca(a,b)
         return self.wei[a] + self.wei[b] - 2*self.wei[lca]
+
+# オイラーツアー
+class DubTree:
+    def __init__(self,N):
+        self.N, self.K = N, 1
+        while (1<<self.K)<N : self.K += 1
+        self.node,self.wei = [None]*self.N, [None]*self.N
+        self.edge = [[] for n in range(self.N)]
+        self.parent = [[None]*N for k in range(self.K)]
+        self.euler = []
+        self.eufl = [[0,0] for n in range(N)]
+        self.fent = []
+    def append_edge(self,a,b,w=1):
+        self.edge[a].append((b,w)); self.edge[b].append((a,w))
+    def dfs_dub(self,s):
+        stack, self.node[s],self.wei[s] = [s], 0, 0
+        self.parent[0][s] = None
+        self._dfs(s)
+        self._doubling()
+        self.fent = [0]*(len(self.euler)+1)
+    def _dfs(self,pos):
+        self.euler.append(pos)
+        self.eufl[pos][0] = len(self.euler)
+        for p,w in self.edge[pos]:
+            if self.node[p] is None:
+                self.node[p], self.wei[p] = self.node[pos]+1, self.wei[pos]+w
+                self.parent[0][p] = pos
+                self._dfs(p)
+                self.euler.append(pos)
+        self.eufl[pos][1] = len(self.euler)
+    def _doubling(self):
+        for k in range(self.K-1):
+            for n in range(self.N):
+                if self.parent[k][n] is None : self.parent[k+1][n] = None
+                else : self.parent[k+1][n] = self.parent[k][self.parent[k][n]]
+    def lca(self,a,b):
+        if self.node[a]<self.node[b] : a,b=b,a
+        for k in range(self.K-1,-1,-1):
+            if self.node[a]-self.node[b]>=(1<<k) : a = self.parent[k][a]
+        if a==b : return a
+        for k in range(self.K-1,-1,-1):
+            if self.parent[k][a]!=self.parent[k][b] : 
+                a,b = self.parent[k][a],self.parent[k][b]
+        return self.parent[0][a]
+    def dist(self,a,b):
+        lca = self.lca(a,b)
+        alen = self.wei[a] + self._sum(self.eufl[a][0]) - self.wei[lca] - self._sum(self.eufl[lca][0])
+        blen = self.wei[b] + self._sum(self.eufl[b][0]) - self.wei[lca] - self._sum(self.eufl[lca][0])
+        return alen + blen
+    def add(self,a,i):
+        n = self.eufl[a][0]
+        while n<=len(self.fent)-1:
+            self.fent[n]+=i
+            n += n&-n
+        n = self.eufl[a][1]+1
+        while n<=len(self.fent)-1:
+            self.fent[n]-=i
+            n += n&-n
+    def _sum(self,n):
+        ans = 0
+        while n>0:
+            ans += self.fent[n]
+            n -= n&-n
+        return ans
