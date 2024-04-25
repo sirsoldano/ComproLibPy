919eb9fac3b04a5c48c8561e61f211001e5c219f
Daiki Soma <fg.strato@gmail.com>
2024-04-25 13:06:47 +0900

    Update Linear.py

--- algorithm/Linear.py
+++ algorithm/Linear.py
@@ -15,3 +15,20 @@ class Linear:
                 for j in range(len(a)):
                     c[i][j] = (c[i][j]+a[i][k]*b[k][j])%self.mod
         return c
+
+def matexp(a,r,b,mod):
+    n = len(a); m = r.bit_length()
+    res = [a]+[[[0]*n for _ in range(n)] for _ in range(m-1)]
+    for h in range(m-1):
+        for i in range(n):
+            for j in range(n):
+                for k in range(n): res[h+1][i][j] += res[h][i][k]*res[h][k][j]%mod
+                res[h+1][i][j] %= mod
+    for h in range(m):
+        if not r>>h&1: continue
+        tmp = [0]*n
+        for i in range(n):
+            for j in range(n): tmp[i] += res[h][i][j]*b[j]%mod
+            tmp[i] %= mod
+        b = tmp
+    return b
