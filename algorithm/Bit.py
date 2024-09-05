891b9c2cf1912220f262ce22100114439c2035aa
Daiki Soma <fg.strato@gmail.com>
2024-09-06 08:24:17 +0900

    Update Bit.py

--- algorithm/Bit.py
+++ algorithm/Bit.py
@@ -37,3 +37,13 @@ for i in range(1<<N):
     while subbit>0:
         subbit = (subbit-1) & i
         bit[i].append(subbit)
+
+# popcount https://qiita.com/zawawahoge/items/8bbd4c2319e7f7746266
+def popcount(x):
+    x = x - ((x >> 1) & 0x5555555555555555)
+    x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333)
+    x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f
+    x = x + (x >> 8)
+    x = x + (x >> 16)
+    x = x + (x >> 32)
+    return x & 0x0000007f
