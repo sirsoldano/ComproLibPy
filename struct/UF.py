5b5d1b274e36d5e5dd283c254b3f2c981df9d11f
Daiki Soma <fg.strato@gmail.com>
2024-10-16 18:24:26 +0900

    Update UF.py

--- struct/UF.py
+++ struct/UF.py
@@ -1,12 +1,11 @@
 # 階層なしUF木 https://atcoder.jp/contests/typical90/tasks/typical90_l
-import sys
-sys.setrecursionlimit(10**6)
+import sys; sys.setrecursionlimit(10**6); import pypyjit; pypyjit.set_param('max_unroll_recursion=-1')
 class UnionFind:
-    def __init__(self,n):
-        self.uft = [-1]*n
-        self.rank = [0]*n
+    def __init__(self,N):
+        self.uft = [n for n in range(N)]
+        self.rank = [0]*N
     def root(self,pos):
-        if self.uft[pos] == -1 : return pos
+        if self.uft[pos] == pos : return pos
         self.uft[pos]=self.root(self.uft[pos])
         return self.uft[pos]
     def union(self,a,b):
